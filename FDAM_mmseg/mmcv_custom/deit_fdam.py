e9512fa5b4a382bb55139070e27c9dc0385c866e
CharlesChen <charleschen2013@163.com>
2025-10-10 18:29:25 +0800

    ...

--- FDAM_mmseg/mmcv_custom/deit_fdam.py
+++ FDAM_mmseg/mmcv_custom/deit_fdam.py
@@ -1,7 +1,5 @@
 # --------------------------------------------------------
-# InternVL
-# Copyright (c) 2024 OpenGVLab
-# Licensed under The MIT License [see LICENSE for details]
+# Linwei Chen
 # --------------------------------------------------------
 import math
 import torch
